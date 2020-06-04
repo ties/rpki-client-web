e830d6fb267ebd7dbd7feca943ca72c0cbf1a628
Ties de Kock <ties@tiesdekock.nl>
2020-06-04 10:44:04 +0200

    Switch test to pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,8 @@
 import os.path
 import tempfile
-from typing import Dict
-from unittest import TestCase
+import pytest
 
+from typing import Dict
 from yaml import Loader, dump, load
 
 from rpkiclientweb.rpki_client import RpkiClient
@@ -19,49 +19,48 @@ def load_sample_conf() -> Dict:
     return conf
 
 
-class ConfigTests(TestCase):
-    def test_config_checks_cache_dir(self):
-        conf = load_sample_conf()
+def test_config_checks_cache_dir():
+    conf = load_sample_conf()
 
-        with tempfile.TemporaryDirectory() as dir_name:
-            conf["output_dir"] = dir_name
-            conf["cache_dir"] = ".well-known-missing"
+    with tempfile.TemporaryDirectory() as dir_name:
+        conf["output_dir"] = dir_name
+        conf["cache_dir"] = ".well-known-missing"
 
-            with self.assertRaises(ValueError):
-                client = RpkiClient(**conf)
-                client.args
-
-    def test_config_checks_output_dir(self):
-        conf = load_sample_conf()
-
-        with tempfile.TemporaryDirectory() as dir_name:
-            conf["cache_dir"] = dir_name
-            conf["output_dir"] = ".well-known-missing"
-
-            with self.assertRaises(ValueError):
-                client = RpkiClient(**conf)
-                client.args
-
-    def test_config_accepts_when_both_exist(self):
-        conf = load_sample_conf()
-
-        with tempfile.TemporaryDirectory() as dir_name:
-            with tempfile.TemporaryDirectory() as another_dir_name:
-                conf["cache_dir"] = dir_name
-                conf["output_dir"] = another_dir_name
-
-                client = RpkiClient(**conf)
-                client.args
-
-    def test_requires_rpki_client_present(self):
-        conf = load_sample_conf()
-
-        conf["rpki_client"] = "/bin/bash"
-
-        client = RpkiClient(**conf)
-        client.args
-
-        with self.assertRaises(ValueError):
-            conf["rpki_client"] = "/.well-known-missing"
+        with pytest.raises(ValueError):
             client = RpkiClient(**conf)
             client.args
+
+def test_config_checks_output_dir():
+    conf = load_sample_conf()
+
+    with tempfile.TemporaryDirectory() as dir_name:
+        conf["cache_dir"] = dir_name
+        conf["output_dir"] = ".well-known-missing"
+
+        with pytest.raises(ValueError):
+            client = RpkiClient(**conf)
+            client.args
+
+def test_config_accepts_when_both_exist():
+    conf = load_sample_conf()
+
+    with tempfile.TemporaryDirectory() as dir_name:
+        with tempfile.TemporaryDirectory() as another_dir_name:
+            conf["cache_dir"] = dir_name
+            conf["output_dir"] = another_dir_name
+
+            client = RpkiClient(**conf)
+            client.args
+
+def test_requires_rpki_client_present():
+    conf = load_sample_conf()
+
+    conf["rpki_client"] = "/bin/bash"
+
+    client = RpkiClient(**conf)
+    client.args
+
+    with pytest.raises(ValueError):
+        conf["rpki_client"] = "/.well-known-missing"
+        client = RpkiClient(**conf)
+        client.args
