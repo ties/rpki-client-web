48aa47f5ead002653f8dd39777c82de9ac113b20
Ties de Kock <ties@tiesdekock.nl>
2021-02-08 12:16:01 +0100

    Track vanished and revoked warnings

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, "README.md"), "rb") as f:
 
 setup(
     name="rpkiclientweb",
-    version="0.6.2",
+    version="0.7.0",
     author="Ties de Kock",
     author_email="ties@tiesdekock.nl",
     description="A web api for RPKI-client",
